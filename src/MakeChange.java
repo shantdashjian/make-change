8c0559fafb8a047d97cca50df323f0540f094a23
Shaun Dashjian <shaundashjian@yahoo.com>
2017-02-04 22:30:26 -0700

    User Story #1: The user is prompted asking for the price of the item.

---
+++ src/MakeChange.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MakeChange {
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		double amount;
+		System.out.print("Please enter the price of the item: ");
+		amount = keyboard.nextDouble();
+	}
+}

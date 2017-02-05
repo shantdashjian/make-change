dc1c2e99a2149e312428358dd93e84895353ad71
Shaun Dashjian <shaundashjian@yahoo.com>
2017-02-04 22:33:58 -0700

    User Story #2: The user is then prompted asking how much money was tendered by the customer.

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -4,7 +4,11 @@ public class MakeChange {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		double amount;
+		double tendered;
 		System.out.print("Please enter the price of the item: ");
 		amount = keyboard.nextDouble();
+		System.out.print("How much money was tendered by the customer? ");
+		tendered = keyboard.nextDouble();
+
 	}
 }
